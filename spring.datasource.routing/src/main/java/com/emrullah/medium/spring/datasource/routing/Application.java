8c06b02963971b472f4536584c0a2ebe6abbbf34
Emrullah YILDIRIM <47065385+Sangaibisi@users.noreply.github.com>
2021-01-30 01:50:06 +0300

    İnitial commit

---
+++ spring.datasource.routing/src/main/java/com/emrullah/medium/spring/datasource/routing/Application.java
@@ -0,0 +1,13 @@
+package com.emrullah.medium.spring.datasource.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
